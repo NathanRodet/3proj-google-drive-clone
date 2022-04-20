286eb17b99870f79aa7620f1a06af9ca2b1c868d
Nathan Rodet <rodetnathan.contact@gmail.com>
2022-04-20 17:33:03 +0200

    Added app resource in terraform for backend

--- Scripts/Terraform/main.tf
+++ Scripts/Terraform/main.tf
@@ -2,8 +2,31 @@ resource "azurerm_storage_account" "stosupinfo3projXXX" {
   name                     = join("", ["sto", "supinfo", "3proj", var.environment])
   resource_group_name      = var.resource_group_name
   location                 = var.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  min_tls_version           = "TLS1_2"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_app_service_plan" "plan_supinfo_3proj_XXX" {
+  name                = join("", ["plan", "-", "supinfo", "-", "3proj", "-", var.environment])
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_supinfo_3proj_XXX" {
+  name                = join("", ["app", "-", "supinfo", "-", "3proj", "-", var.environment])
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.plan_supinfo_3proj_XXX.id
+
+  site_config {
+    always_on       = true
+    min_tls_version = "1.2"
+  }
 }
