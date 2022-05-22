c68d867fe121420926e50dd647c44eaaae383076
NathanRodet <nathan.rodet@hardis-group.com>
2022-05-22 22:41:32 +0200

    Full working authentification, protected routes

--- 3proj-frontend/src/App.js
+++ 3proj-frontend/src/App.js
@@ -38,15 +38,27 @@ export default function App() {
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/Home"} element={<Home />} />
-        <Route path={"/Dashboard"} element={<Dashboard />} />
-        <Route path={"/AdminDashboard"} element={<AdminDashboard />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/SignOut" element={<SignOut />} />
-        <Route path="/ForgotPassword" element={<ForgotPassword />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-        <Route path="/Documentation" element={<Documentation />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path={"/SignIn"} element={<SignIn />} />
+        <Route path={"/SignUp"} element={<SignUp />} />
+        {
+          (isAdmin || isLogged) && (
+            <>
+              <Route path={"/Dashboard"} element={<Dashboard />} />
+              <Route path={"/SignOut"} element={<SignOut />} />
+              <Route path={"/ForgotPassword"} element={<ForgotPassword />} />
+            </>
+          )
+        }
+        {
+          (isAdmin) && (
+            <>
+              <Route path={"/AdminDashboard"} element={<AdminDashboard />} />
+            </>
+          )
+        }
+        <Route path={"/ContactUs"} element={<ContactUs />} />
+        <Route path={"/Documentation"} element={<Documentation />} />
+        <Route path={"*"} element={<NotFound />} />
       </Routes>
       <footer className="footer">
         <Footer />
