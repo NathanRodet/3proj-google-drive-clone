50d8999f6e368b212cbad0a69e0372ebdc417341
NathanRodet <nathan.rodet@hardis-group.com>
2022-06-02 21:34:39 +0200

    Added Admindashboard table

--- 3proj-frontend/src/services/auth/auth.js
+++ 3proj-frontend/src/services/auth/auth.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://api-supinfo-3proj-dev.azurewebsites.net";
 
 export default async function postAuth(data) {
   return axios.post(API_URL + "/auth", data)
     .then(response => {
       // console.log(response)
-      return response
+      return response.data
     })
     .catch(error => {
       // console.log(error.response.data);
