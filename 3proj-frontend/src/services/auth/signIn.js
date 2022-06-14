cfb88665b1fdb6df75ee02b533c6792f17c001fa
NathanRodet <nathan.rodet@hardis-group.com>
2022-06-15 00:33:13 +0200

    no message

--- 3proj-frontend/src/services/auth/signIn.js
+++ 3proj-frontend/src/services/auth/signIn.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
+const API_URL = process.env.BASEURL || "http://localhost:3001";
+console.log(API_URL);
+console.log(process.env.BASEURL);
 
 export default async function postAuth(data) {
   return axios.post(API_URL + "/auth", data)
